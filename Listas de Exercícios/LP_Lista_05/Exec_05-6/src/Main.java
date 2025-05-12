c62ead8c0bb3dbba121ac76b289f75b7305c1485
Caiq <caiqueleandrotessaroto@gmail.com>
2025-05-12 20:05:47 -0300

    Lista 5 Correção 6/12

--- Listas de Exercícios/LP_Lista_05/Exec_05-6/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/Exec_05-6/src/Main.java
@@ -3,17 +3,25 @@ public class Main {
         //6. Declare e inicialize um vetor, em seguida inverta a sequência dos valores armazenados neste mesmo vetor.
 
         int[] vetor = new int[10];
-        int[] mirror_vetor = new int[10];
+        //int[] mirror_vetor = new int[10];
 
+        int v;
 
         for (int i = 0; i < vetor.length ; i++) //Declarações do vetor
             vetor[i] = i + 1;
 
-        for (int i = 0; i < vetor.length ; i++)//Espelhamento(Inversão) em mirro_vetor
-            mirror_vetor[i] = vetor[vetor.length -1 - i];
+        for (int i = 0; i < vetor.length / 2 ; i++) {
+            v = vetor[i];
+            vetor[i] = vetor[vetor.length - 1 - i];
+            vetor[vetor.length - 1 - i] = v;
+        }
 
-        for (int i = 0; i < vetor.length ; i++)//Re declaração agora invertido
-            vetor[i] = mirror_vetor[i];
+        //for (int i = 0; i < vetor.length ; i++) {//Espelhamento(Inversão) em mirro_vetor
+        //    mirror_vetor[i] = vetor[vetor.length - 1 - i];
+        //}
+
+        //for (int i = 0; i < vetor.length ; i++)//Re declaração agora invertido
+        //    vetor[i] = mirror_vetor[i];
 
         for (int i = 0; i < vetor.length ; i++)
             System.out.println(vetor[i]);
